d827675b96a5103e44c6188af9b08a651426d612
Tracy Lee <tracy@wingman.events>
2016-08-03 11:58:54 -0700

    run 2 works!

--- src/app/form-demo/form-demo.component.ts
+++ src/app/form-demo/form-demo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Submissions } from '.submissions/model';
 
 @Component({
   selector: 'app-form-demo',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormDemoComponent implements OnInit {
 
+  submissions: Submissions = { firstName: '', lastName: '', email: '' };
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  submitForm(myForm:NgForm) {
+    alert(JSON.stringify(myForm.value));
+  }
 }
