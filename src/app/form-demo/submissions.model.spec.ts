5c553eb665080767f830afb904c1306474edd071
Tracy Lee <tracy@wingman.events>
2016-08-03 11:32:48 -0700

    my first run and it worked!

---
+++ src/app/form-demo/submissions.model.spec.ts
@@ -0,0 +1,10 @@
+/* tslint:disable:no-unused-variable */
+
+import { addProviders, async, inject } from '@angular/core/testing';
+import {Submissions} from './submissions.model';
+
+describe('Submissions', () => {
+  it('should create an instance', () => {
+    expect(new Submissions()).toBeTruthy();
+  });
+});
---
+++ src/app/form-demo/submissions.model.ts
@@ -0,0 +1,5 @@
+export class Submissions {
+  firstName: string,
+  lastName: string,
+  email: string
+}
